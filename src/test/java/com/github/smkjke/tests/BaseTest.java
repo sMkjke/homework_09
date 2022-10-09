9510b812a35f78b349c831dc981f8e4e29c6890c
Mikhail Voronov <smirk06@yandex.ru>
2022-10-09 22:52:10 +0300

    homework 09

--- src/test/java/com/github/smkjke/tests/BaseTest.java
+++ src/test/java/com/github/smkjke/tests/BaseTest.java
@@ -3,6 +3,8 @@ package com.github.smkjke.tests;
 import com.codeborne.selenide.Configuration;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.smkjke.helpers.Attachments;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import io.qameta.allure.selenide.AllureSelenide;
 
@@ -14,6 +16,13 @@ public class BaseTest {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browserSize = "1920x1080";
+    }
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
     }
 
 }
---
+++ src/test/java/com/github/smkjke/tests/DemoqaPracticeFormTestWithParameters.java
@@ -0,0 +1,50 @@
+package com.github.smkjke.tests;
+
+import com.github.smkjke.pages.RegistrationFormPage;
+import com.github.smkjke.testData.FakerUser;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class DemoqaPracticeFormTestWithParameters extends BaseTest {
+
+    private final RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    @Test
+    void fillDemoqaAutomationPracticeFormTestWithParams() {
+        step("Enter user data", () ->
+        {
+            registrationFormPage.openPage()
+                    .setFirstName(FakerUser.firstName)
+                    .setLastName(FakerUser.lastName)
+                    .setEmail(FakerUser.email)
+                    .setGender(FakerUser.gender)
+                    .setPhoneNumber(FakerUser.phoneNumber)
+                    .setBirthDate(FakerUser.dateOfBirth.getDayOfMonth(),
+                            String.valueOf(FakerUser.dateOfBirth.getMonthValue() - 1),
+                            String.valueOf(FakerUser.dateOfBirth.getYear()))
+                    .setSubjects(FakerUser.subjects)
+                    .setHobbies(FakerUser.hobbies)
+                    .uploadFile(FakerUser.file)
+                    .setCurrentAddress(FakerUser.currentAddress)
+                    .setState(FakerUser.state)
+                    .setCity(FakerUser.city)
+                    .submit();
+        });
+
+        step("Checks form is correct", () -> {
+            registrationFormPage.checkResultsTableVisible();
+            registrationFormPage.checkResult("Student Name", FakerUser.firstName + " " + FakerUser.lastName)
+                    .checkResult("Student Email", FakerUser.email)
+                    .checkResult("Gender", FakerUser.gender)
+                    .checkResult("Mobile", FakerUser.phoneNumber)
+                    .checkResult("Date of Birth", FakerUser.getFullBirthdayDateWithFormat())
+                    .checkResult("Subjects", FakerUser.subjects.toString().replace("[", "").replace("]", ""))
+                    .checkResult("Hobbies", FakerUser.hobbies.toString().replace("[", "").replace("]", ""))
+                    .checkResult("Picture", FakerUser.file.getName())
+                    .checkResult("Address", FakerUser.currentAddress)
+                    .checkResult("State and City", FakerUser.state + " " + FakerUser.city);
+        });
+    }
+
+}
