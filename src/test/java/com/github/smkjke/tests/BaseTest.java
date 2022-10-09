3216fc6d9f2dc07c9b9def1a123e6220001980da
Mikhail Voronov <smirk06@yandex.ru>
2022-10-09 22:23:04 +0300

    ref3

--- src/test/java/com/github/smkjke/tests/BaseTest.java
+++ src/test/java/com/github/smkjke/tests/BaseTest.java
@@ -11,6 +11,7 @@ public class BaseTest {
     @BeforeEach
     void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browserSize = "1920x1080";
     }
