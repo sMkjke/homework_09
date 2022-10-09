97e956c7a21e641853deef618f7eeeb90a4de8a9
Mikhail Voronov <smirk06@yandex.ru>
2022-10-09 22:59:45 +0300

    homework 09 + add video ref

--- src/test/java/com/github/smkjke/tests/BaseTest.java
+++ src/test/java/com/github/smkjke/tests/BaseTest.java
@@ -16,11 +16,13 @@ public class BaseTest {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
+
 //        Configuration.browserSize = "1920x1080";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
     @AfterEach
     void addAttachments() {
