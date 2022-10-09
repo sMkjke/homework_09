93cbafbf5335f9213695cc5d2300782c9aefd667
Mikhail Voronov <smirk06@yandex.ru>
2022-10-09 22:11:21 +0300

    ref2

--- src/test/java/com/github/smkjke/tests/BaseTest.java
+++ src/test/java/com/github/smkjke/tests/BaseTest.java
@@ -1,12 +1,16 @@
 package com.github.smkjke.tests;
 
 import com.codeborne.selenide.Configuration;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class BaseTest {
 
     @BeforeEach
     void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browserSize = "1920x1080";
     }
